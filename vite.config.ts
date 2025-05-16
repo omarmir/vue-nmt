84ec4a4158c5d466af2cbc203a4631930ef4137e
Omar Mir <omarmir@gmail.com>
2025-05-15 23:08:07 -0400

    build fixed

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   appType: 'mpa',
+  build: {
+    target: 'esnext',
+  },
   plugins: [
     vue(),
     vueDevTools(),
@@ -50,5 +53,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  worker: {
+    format: 'es',
   },
 })
