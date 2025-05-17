8c2d6358a3e9005b99c0a5bd960eb9b6bbe550e2
Omar Mir <omarmir@gmail.com>
2025-05-16 23:37:05 -0400

    loading indicator

--- src/components/TextTranslate.vue
+++ src/components/TextTranslate.vue
@@ -19,6 +19,29 @@
       v-model="store.generationParams"
       v-model:threads="store.maxConcurrentWorkers"
     ></GenerationConfig>
+    <div
+      class="flex flex-col gap-2 w-full items-center place-content-center"
+      v-if="store.isTranslating"
+    >
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <path
+          fill="currentColor"
+          d="M12 2A10 10 0 1 0 22 12A10 10 0 0 0 12 2Zm0 18a8 8 0 1 1 8-8A8 8 0 0 1 12 20Z"
+          opacity="0.5"
+        />
+        <path fill="currentColor" d="M20 12h2A10 10 0 0 0 12 2V4A8 8 0 0 1 20 12Z">
+          <animateTransform
+            attributeName="transform"
+            dur="1s"
+            from="0 12 12"
+            repeatCount="indefinite"
+            to="360 12 12"
+            type="rotate"
+          />
+        </path>
+      </svg>
+      Translating ...
+    </div>
     <div class="whitespace-pre-wrap">
       {{ store.outputText }}
     </div>
