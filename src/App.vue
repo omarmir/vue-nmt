4311e57980cce990eb93cb0279b4060e70a401b2
Omar Mir <omarmir@gmail.com>
2025-05-21 11:20:33 -0400

    Adding a quick credit to the app

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,17 @@
   <div class="flex flex-col max-w-6xl m-auto mt-10 gap-10 px-5">
     <header>
       <h1 class="text-2xl font-bold">Neural Machine Translation (NMT): English to French</h1>
+      <small>
+        All data (text or files) remain on your computer. This is a client side application.
+        <br />
+        Created by
+        <a
+          target="_blank"
+          href="https://github.com/omarmir/"
+          class="text-blue-500 hover:underline hover:text-blue-700"
+          >Omar Mir</a
+        >
+      </small>
     </header>
     <RouterView />
   </div>
